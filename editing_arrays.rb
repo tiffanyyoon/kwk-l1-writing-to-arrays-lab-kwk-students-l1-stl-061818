1cc846cee5e38ac10da8a43361b47f12bcf1815e
tiffanyyoon <yyoon032@rsdmo.org>
2018-06-20 16:11:58 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,7 +6,9 @@ rainbow_colors = ["yellow","default","light_cyan"]
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
   rainbow_colors.push("green", "blue")
 
-
+def change_rainbow_colors
+  rainbow_colors
+end
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 
 puts rainbow_colors
